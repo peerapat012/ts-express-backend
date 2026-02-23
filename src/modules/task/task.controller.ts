d03565ebf24e1945f1ea278357c15a8a3a81a266
Peerapat Srisarungkarn <peerapat.jacky@gmail.com>
2026-02-23 19:13:58 +0700

    Add task update and delete controller

--- src/modules/task/task.controller.ts
+++ src/modules/task/task.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import * as taskService from "./task.service";
+import { z } from "zod";
+import { TaskStatus } from "./task.types";
 
 export const getAllTasks = async (req: Request, res: Response) => {
     try {
@@ -20,3 +22,24 @@ export const createTask = async (req: Request, res: Response) => {
         res.status(400).json({ message: error.message });
     }
 };
+
+export const updateStatusTask = async (req: Request, res: Response) => {
+    try {
+        const { taskId, status } = req.body;
+        z.enum(TaskStatus).parse(status);
+        const updatedTask = await taskService.updateTaskStatus(taskId, status);
+        res.json(updatedTask);
+    } catch (error: any) {
+        res.status(400).json({ message: error.message });
+    }
+}
+
+export const deleteTask = async (req: Request, res: Response) => {
+    try {
+        const { taskId } = req.body;
+        await taskService.deleteTask(taskId);
+        res.status(204).send();
+    } catch (error: any) {
+        res.status(400).json({ message: error.message });
+    }
+}
--- src/modules/task/task.route.ts
+++ src/modules/task/task.route.ts
@@ -5,5 +5,7 @@ const router = Router();
 
 router.get("/", taskController.getAllTasks);
 router.post("/", taskController.createTask);
+router.put("/status", taskController.updateStatusTask);
+router.delete("/", taskController.deleteTask);
 
 export default router;
