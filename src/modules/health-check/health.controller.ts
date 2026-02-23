c587e95dcb06c82409a8d2a087ea3d78bee60edd
Peerapat Srisarungkarn <peerapat.jacky@gmail.com>
2026-02-23 14:47:44 +0700

    Delete not use module

--- src/modules/health-check/health.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-
-export const healthCheck = (req: Request, res: Response) => {
-    res.status(200).json({
-        status: "OK",
-        message: "Server is running successfully!"
-    });
-};
